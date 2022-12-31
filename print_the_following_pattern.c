3fb056f86cea3f127237e7a4b9f60a665ec18ecc
umavenkatbande <119839657+umavenkatbande@users.noreply.github.com>
2022-12-31 12:16:58 +0530

    #include<stdio.h> int main() {     int n,i,j,c=0;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=n;j>=1;j--)         {             c++;             printf("%d ",j);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,j,c=0;
-	scanf("%d",&n);
-	for(i=n;i>=1;i--)
-	{
-	    for(j=1;j<=i;j++)
-		{
-		    c++;
-		    printf("%d",j);
-		}
-		printf("
+    int n,i,j,c=0;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=n;j>=1;j--)
+        {
+            c++;
+            printf("%d ",j);
+        }
+        printf("
 ");
-	}
+    }
 }
