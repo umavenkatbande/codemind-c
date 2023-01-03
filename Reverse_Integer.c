2c0859b32b3e58a9f5f8dbc647ffc977bd6b063d
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-03 11:26:11 +0530

    #include<stdio.h> int main() {     int i,n,s=0,q,r;     scanf("%d",&n);     while(n!=0)
        {
            r=n%10;
            s=s*10+r;
            n=n/10; 
            
        } 
        printf("%d",s);
        
    }

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
-int is_palndrm(int);
 int main()
-{
-	int n;
-	scanf("%d",&n);
-	printf("%d",is_palndrm(n));	
-}
-int is_palndrm(int m)
-{
-	int q,r,s=0;
-	q=m;
-	while(q!=0)
-	{
-		r=q%10;
-		s=s*10+r;
-		q=q/10;	
-	}
-	return s;
+{ 
+    int i,n,s=0,q,r;
+    scanf("%d",&n);
+    while(n!=0)
+    
+    {
+        r=n%10;
+        s=s*10+r;
+        n=n/10; 
+        
+    } 
+    printf("%d",s);
+    
 }
