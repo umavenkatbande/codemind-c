7b4640a343b25dd355177e547eefa977a4461883
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-03 11:00:28 +0530

    #include<stdio.h> int main() {     long long int a,b,l,lcm,hcf=0;     scanf("%lld%lld",&a,&b);     for(l=1;l<=a&&l<=b;l++)     {         if(a%l==0 && b%l==0)         {             hcf=l;         }
        } 
        lcm=a*b/hcf;
        printf("%lld",lcm);
        
    }

--- Least_Common_Multiple.c
+++ Least_Common_Multiple.c
@@ -3,13 +3,15 @@ int main()
 {
     long long int a,b,l,lcm,hcf=0;
     scanf("%lld%lld",&a,&b);
-    for(l=1;l<=a&&l<=b;l++)
-    {
+    for(l=1;l<=a&&l<=b;l++) 
+    { 
         if(a%l==0 && b%l==0)
         {
             hcf=l;
-        }
-    }
-     lcm=a*b/hcf;
-     printf("%lld",lcm);
+        } 
+        
+    } 
+    lcm=a*b/hcf;
+    printf("%lld",lcm);
+    
 }
