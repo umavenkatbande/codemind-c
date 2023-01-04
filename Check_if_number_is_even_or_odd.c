ad3d38ab597242df898f42eeccf03676e8d3bcab
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-04 18:14:15 +0530

    #include<stdio.h> int main() {     int a,n;     scanf("%d",&a);     n=a%2;     if(n==1)     {         printf("Odd");     }     else     {         printf("Even");     } }

---
+++ Check_if_number_is_even_or_odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    scanf("%d",&a);
+    n=a%2;
+    if(n==1)
+    {
+        printf("Odd");
+    }
+    else
+    {
+        printf("Even");
+    }
+}
