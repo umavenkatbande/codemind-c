2db736636f613b2b5ef90b8c19915a9dab665dde
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-05 20:23:42 +0530

    #include<stdio.h> void hms(int a) {     int b,c,d,e;     b=a/3600;     c=a-(b*3600);     d=c/60;     e=c%60;     printf("H:M:S-%d:%d:%d",b,d,e);
    }
    int main()
    { 
        int a;
        scanf("%d",&a);
        hms(a);
        
    }

--- convert_a_given_integer_(in_seconds)_to_hours,_minutes_and_seconds..c
+++ convert_a_given_integer_(in_seconds)_to_hours,_minutes_and_seconds..c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 void hms(int a)
-{ 
-    int b,c,d,e;
+{
+    int b,c,d,e; 
     b=a/3600;
     c=a-(b*3600);
-    d=c/60;
+    d=c/60; 
     e=c%60;
     printf("H:M:S-%d:%d:%d",b,d,e);
     
 }
 int main()
-{
-    int a; 
+{ 
+    int a;
     scanf("%d",&a);
     hms(a);
     
