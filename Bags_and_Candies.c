7f9d311c02322634f50e0c51bc9c883282b5e577
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-07 11:28:58 +0530

    #include<stdio.h> int main() {     int n,k,m,cpb;     scanf("%d%d%d",&n,&k,&m);     cpb=k*m;     if(n%cpb==0)     printf("%d",n/cpb);     else     printf("%d",n/cpb+1); }

---
+++ Bags_and_Candies.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,m,cpb;
+    scanf("%d%d%d",&n,&k,&m);
+    cpb=k*m;
+    if(n%cpb==0)
+    printf("%d",n/cpb);
+    else 
+    printf("%d",n/cpb+1);
+}
