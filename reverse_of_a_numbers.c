d569ea7efa6533fdc8f1c5c59efc125a7dd771fa
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-08 19:04:00 +0530

    #include<stdio.h> int main() {     int n,rem,sum=0;     scanf("%d",&n);     while(n>0)     {         rem=n%10;         sum=(sum*10)+rem;         n=n/10;         } printf("%d",sum); }

--- reverse_of_a_numbers.c
+++ reverse_of_a_numbers.c
@@ -2,13 +2,10 @@
 int main()
 {
     int n,rem,sum=0;
-    scanf("%d",&n); 
+    scanf("%d",&n);
     while(n>0) 
     {
-        
         rem=n%10;
-        sum=(sum*10)+rem; n=n/10;
-        }
-        printf("%d",sum); 
-    
-}
+        sum=(sum*10)+rem;
+        n=n/10;
+        } printf("%d",sum); }
