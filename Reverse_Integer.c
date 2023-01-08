abef3270114f097b936cc2780c1976bac11e635d
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-08 19:09:41 +0530

    #include<stdio.h> int main() {     int i,n,s=0,q,r;     scanf("%d",&n);     while(n!=0)     { r=n%10; s=s*10+r; n=n/10; }     printf("%d",s);}

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,16 +1,8 @@
 #include<stdio.h>
 int main()
 { 
-    int i,n,s=0,q,r;
+    int i,n,s=0,q,r; 
     scanf("%d",&n);
     while(n!=0)
-    
-    {
-        r=n%10;
-        s=s*10+r;
-        n=n/10; 
-        
-    } 
-    printf("%d",s);
-    
-}
+    { r=n%10; s=s*10+r; n=n/10; }
+    printf("%d",s);}
