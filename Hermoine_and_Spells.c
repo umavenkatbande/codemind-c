e12e8d68e26a9e358dafc3b86e700929580b28a7
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-11 20:48:03 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(b>a && c>a)     printf("%d",b+c);     else if(a>c && b>c)     printf("%d",a+b);     else     printf("%d",a+c);
    }

--- Hermoine_and_Spells.c
+++ Hermoine_and_Spells.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
+    int a,b,c; 
     scanf("%d%d%d",&a,&b,&c);
     if(b>a && c>a)
     printf("%d",b+c);
     else if(a>c && b>c)
     printf("%d",a+b);
-    else
+    else 
     printf("%d",a+c);
+    
 }
