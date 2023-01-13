53d789004d75961f1a7d4d0ecad182fbce460181
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-13 19:38:11 +0530

    #include<stdio.h> int main() {     int n,i;     float sum=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]); } for(i=0;i<n;i++) { sum=sum+a[i]; } printf("%0.2f",sum/n); }

--- average_of_the_array.c
+++ average_of_the_array.c
@@ -1,18 +1,10 @@
 #include<stdio.h>
 int main()
-{
-    int n,i;
+{ 
+    int n,i; 
     float sum=0;
     scanf("%d",&n);
     int a[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        sum=sum+a[i];
-    }
-    printf("%0.2f",sum/n);
-    
-}
+    for(i=0;i<n;i++) 
+    { 
+        scanf("%d",&a[i]); } for(i=0;i<n;i++) { sum=sum+a[i]; } printf("%0.2f",sum/n); }
