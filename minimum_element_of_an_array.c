f3af7c408d530441679b6c6a90ce0e70460817a6
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-13 19:41:22 +0530

    #include<stdio.h> int main() {     int n,i; scanf("%d",&n); int a[n]; for(i=0;i<n;i++) { scanf("%d",&a[i]); } for(i=0;i<n;i++) {     if(a[0]>a[i]) a[0]=a[i];
    }
    printf("%d",a[0]);
    }

--- minimum_element_of_an_array.c
+++ minimum_element_of_an_array.c
@@ -2,17 +2,14 @@
 int main()
 {
     int n,i;
-    scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        if(a[0]>a[i])
-        a[0]=a[i];
-    }
-    printf("%d",a[0]);
+scanf("%d",&n);
+int a[n];
+for(i=0;i<n;i++)
+{ scanf("%d",&a[i]);
+} for(i=0;i<n;i++) 
+{ 
+    if(a[0]>a[i]) a[0]=a[i]; 
     
+}
+printf("%d",a[0]);
 }
