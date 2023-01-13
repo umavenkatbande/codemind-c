f2463556ee4de330d4608b99e99d19dc829eb0be
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-13 19:43:32 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);
        }
        for(i=0;i<n;i++) 
        {
            if(a[0]<a[i]) a[0]=a[i];
            }
            printf("%d",a[0]);
            }

--- maximum_of_an_array.c
+++ maximum_of_an_array.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main()
-{
+{ 
     int n,i;
-    scanf("%d",&n);
-    int a[n];
+    scanf("%d",&n); 
+    int a[n]; 
     for(i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
+    { 
+        scanf("%d",&a[i]); 
+        
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<n;i++) 
     {
-        if(a[0]<a[i])
-        a[0]=a[i];
-    }
-    printf("%d",a[0]);
-    
-}
+        if(a[0]<a[i]) a[0]=a[i];
+        }
+        printf("%d",a[0]);
+        }
