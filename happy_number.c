d582dca180348bba4af4050168edbd616f33fb37
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-15 22:21:40 +0530

    #include<stdio.h> int main() { 	int n,q,r,s=0; 	scanf("%d",&n); 	q=n; 	while(q>9) 	{ 		s=0; 	while(q!=0) 	{ 		r=q%10; 		s=s+r*r; 		q=q/10; 	} 	if(s>9) 	{ 		q=s; 	}
    	}
    	if(s==1||s==7)
    	{
    		printf("True");
    	}
    	else
    	{
    		printf("False");
    	}
    	
    //	printf("%d",s);
    }

--- happy_number.c
+++ happy_number.c
@@ -1,18 +1,32 @@
 #include<stdio.h>
 int main()
 {
-    int n,temp,r,sum=0;
-    scanf("%d",&n);
-    temp=n;
-    while(temp>9)
-    {
-        sum=0;
-        while(temp!=0)
-        {
-            r=temp%10;
-            sum=sum+r*r;
-            temp=temp/10;
-            
-        }if(sum>9){temp=sum;
-            
-        }}if(sum==1||sum==7){printf("True");}else{printf("False");}}
+	int n,q,r,s=0;
+	scanf("%d",&n);
+	q=n;
+	while(q>9)
+	{
+		s=0;
+	while(q!=0)
+	{
+		r=q%10;
+		s=s+r*r;
+		q=q/10;		
+	}
+	if(s>9)
+	{
+		q=s;
+	}
+
+	}
+	if(s==1||s==7)
+	{
+		printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+	
+//	printf("%d",s);
+}
