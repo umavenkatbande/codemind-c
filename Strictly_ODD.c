c2de28ea9234fa1e776333fd9de5e8c5e7d5b601
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-18 15:42:56 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     int strictly_odd=1;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }         for(i=0;i<n;i++)     {             if(a[i]%2!=0)         {             if(i%2!=1)             {                 strictly_odd=0;                 break;
                }
            }
       }
                if(strictly_odd==1)
                printf("True");
                else
                printf("False");
        
    }

---
+++ Strictly_ODD.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n); 
+    int a[n]; 
+    int strictly_odd=1;
+    for(i=0;i<n;i++)
+    { 
+        scanf("%d",&a[i]);
+    } 
+        for(i=0;i<n;i++)
+    {
+            if(a[i]%2!=0)
+        {
+            if(i%2!=1)
+            { 
+                strictly_odd=0;
+                break; 
+                
+            }
+        }
+   }
+            if(strictly_odd==1)
+            printf("True");
+            else
+            printf("False");
+    
+}
