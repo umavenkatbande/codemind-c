6573f47b94af973a9f4761de53e3b8261a450344
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-18 14:33:33 +0530

    #include<stdio.h> int main() {     int r,a,n,sum=0;     scanf("%d",&n);     a=n;
        while(a!=0)
        { r=a%10; sum=sum*10+r; a=a/10; } if(n==sum) { printf("Palindrome"); } else { printf("Not Palindrome"); }}

--- Palindrome.c
+++ Palindrome.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
 int main()
-{ int r,a,n,sum=0;
-scanf("%d",&n); a=n; while(a!=0) { r=a%10; sum=sum*10+r; a=a/10; } if(n==sum) { printf("Palindrome"); } else { printf("Not Palindrome"); }}
+{
+    int r,a,n,sum=0;
+    scanf("%d",&n);
+    a=n;
+    
+    while(a!=0)
+    { r=a%10; sum=sum*10+r; a=a/10; } if(n==sum) { printf("Palindrome"); } else { printf("Not Palindrome"); }}
