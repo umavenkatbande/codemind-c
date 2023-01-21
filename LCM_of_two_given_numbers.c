97caede446113ed648d57e058302fdc6caeec3ab
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-21 20:25:42 +0530

    #include<stdio.h> int main() { int a,b,l; scanf("%d%d",&a,&b); for(l=1;l<=(a*b);l++) { if(l%a==0&&l%b==0) break; } printf("%d",l);
    }

--- LCM_of_two_given_numbers.c
+++ LCM_of_two_given_numbers.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 int main()
-{
-    int a,b,l; 
-    scanf("%d%d",&a,&b);
-    
-    for(l=1;l<=(a*b);l++) 
-    { if(l%a==0&&l%b==0) break;
-    }
-    printf("%d",l);
+{ int a,b,l; 
+scanf("%d%d",&a,&b);
+for(l=1;l<=(a*b);l++)
+{ if(l%a==0&&l%b==0)
+break;
+} printf("%d",l);
     
 }
