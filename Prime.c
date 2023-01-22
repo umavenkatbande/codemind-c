6b210f73dcc1aaee326217f4fb539f3abd6f34a3
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-01-22 22:21:34 +0530

    #include<stdio.h> int main() { int i,n,count=0; scanf("%d",&n); for (i=1;i<=n;i++) { if(n%i==0) { count++; } } if(count==2) { printf("Prime"); } else { printf("Not Prime"); }}

--- Prime.c
+++ Prime.c
@@ -1,13 +1,6 @@
 #include<stdio.h>
 int main()
-{ int i,n,count=0; 
-scanf("%d",&n);
+{ int i,n,count=0;
+scanf("%d",&n); 
 for (i=1;i<=n;i++)
-{ if(n%i==0) 
-{ count++; }
-} if(count==2) 
-{ printf("Prime");
-} else { printf("Not Prime");
-}
-    
-}
+{ if(n%i==0) { count++; } } if(count==2) { printf("Prime"); } else { printf("Not Prime"); }}
