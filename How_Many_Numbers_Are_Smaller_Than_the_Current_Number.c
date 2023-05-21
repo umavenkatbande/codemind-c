eccc95b11a1f0cfb87f550b5b1b19290a2d93d08
Uma venkata Bande <119839657+umavenkatbande@users.noreply.github.com>
2023-05-21 09:07:32 +0530

    #include <stdio.h> int main(){ int n,i,j; scanf("%d",&n); int nums[n]; for(i=0; i<n; i++) { scanf("%d",&nums[i]); } for(i=0; i<n; i++) { int count=0; for(j=0; j<n; j++) { if(nums[j] < nums[i]) { count++; } } printf("%d ",count); }}

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main(){ int n,i,j; scanf("%d",&n); int nums[n]; for(i=0; i<n; i++) { scanf("%d",&nums[i]); } for(i=0; i<n; i++) { int count=0; for(j=0; j<n; j++) { if(nums[j] < nums[i]) { count++; } } printf("%d ",count); }}
